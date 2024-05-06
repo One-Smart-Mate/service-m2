e36855891ccc3baf5a5994a1d992cdde233d2d4d
Immanuel Díaz <42914128+imadiaz@users.noreply.github.com>
2024-05-06 13:50:39 -0600

    setup database (#1) (#2)
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import typeOrmConfig from './config/type.orm.config';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    typeOrmConfig,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
