c184840786ed9bf14a0effa52697e073d14d06c4
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-05-07 17:49:17 -0600

    added Swagger dependency and configuration on main.ts file (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function main() {
   const app = await NestFactory.create(AppModule,{ cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('M2 example')
+    .setDescription('M2 API description')
+    .setVersion('1.0')
+    .addTag('M2')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
 
   await app.listen(process.env.PORT || 3000);
 }
