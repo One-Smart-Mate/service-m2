994f06b9355e5b9bb2c8e9f76533f4e47e585e85
isai3 <20223tn137@utez.edu.mx>
2024-05-13 16:41:21 -0600

    Add Global Interceptor, app.enableCors(), Moved string.constants.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function main() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -13,9 +14,7 @@ async function main() {
     .addTag('M2')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  app.enableCors()
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -23,6 +22,9 @@ async function main() {
       transform: true,
     }),
   );
+  app.useGlobalInterceptors(new TransformInterceptor())
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
