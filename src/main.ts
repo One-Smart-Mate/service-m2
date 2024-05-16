04c990d7f1f25077e216bd87e27cd7e6d34e4621
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-05-16 09:08:55 -0600

    Feature/wma 30/custom exceptions (#10)
    * setup database (#1)
    
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>
    
    * update templates (#3)
    
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>
    
    * added Swagger dependency and configuration on main.ts file (#5)
    
    * feature/WMA-5/Login-api (#6)
    
    * Add Login and ResetPassword services
    
    * Corrections done
    
    * feature/WMA-12/User-roles (#7)
    
    * User Roles Implemented in Auth service
    
    * type.orm.config.ts edited, roles.module deleted, and getUserRole method in users.service.ts updated
    
    * Custom Eexceptions Implmented
    
    ---------
    
    Co-authored-by: Immanuel Díaz <42914128+imadiaz@users.noreply.github.com>
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function main() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -14,7 +15,7 @@ async function main() {
     .addTag('M2')
     .addBearerAuth()
     .build();
-  app.enableCors()
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -22,7 +23,8 @@ async function main() {
       transform: true,
     }),
   );
-  app.useGlobalInterceptors(new TransformInterceptor())
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
