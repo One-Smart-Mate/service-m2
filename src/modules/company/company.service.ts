a4081272f4ce97377af2247550fc7ba340eb62eb
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-05-17 12:34:06 -0600

    Feature/wma 22/update company api (#15)
    * setup database (#1)
    
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>
    
    * update templates (#3)
    
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>
    
    * added Swagger dependency and configuration on main.ts file (#5)
    
    * feature/WMA-5/Login-api (#6)
    
    * Add Login and ResetPassword services
    
    * Corrections done
    
    * feature/WMA-12/User-roles (#7)
    
    * User Roles Implemented in Auth service
    
    * type.orm.config.ts edited, roles.module deleted, and getUserRole method in users.service.ts updated
    
    * Update company service implemented
    
    ---------
    
    Co-authored-by: Immanuel Díaz <42914128+imadiaz@users.noreply.github.com>
    Co-authored-by: Immanuel Diaz <immanuel.diaz@includedhealth.com>

--- src/modules/company/company.service.ts
+++ src/modules/company/company.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCompanyDTO } from './dto/create-company.dto';
+import { CreateCompanyDTO } from './models/dto/create-company.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CompanyEntity } from './entities/company.entity';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { HandleException } from 'src/common/exceptions/handler/handle.exception';
 import {
   ValidationException,
   ValidationExceptionType,
 } from 'src/common/exceptions/types/validation.exception';
-import { NotFoundCustomException, NotFoundCustomExceptionType } from 'src/common/exceptions/types/notFound.exception';
+import {
+  NotFoundCustomException,
+  NotFoundCustomExceptionType,
+} from 'src/common/exceptions/types/notFound.exception';
+import { UpdateCompanyDTO } from './models/dto/update-company.dto';
 
 @Injectable()
 export class CompanyService {
@@ -39,25 +43,65 @@ export class CompanyService {
   };
 
   findAllCompanies = async () => {
-    try{
-      return await this.companyRepository.find()
-    }catch(exception){
-      HandleException.exception(exception)
+    try {
+      return await this.companyRepository.find();
+    } catch (exception) {
+      HandleException.exception(exception);
     }
-  }
+  };
 
-  findCompanyById = async (id: number) =>{
-    try{
-      const companyExists = await this.companyRepository.existsBy({id: id})
+  findCompanyById = async (id: number) => {
+    try {
+      const companyExists = await this.companyRepository.existsBy({ id: id });
 
-      if(!companyExists){
-        throw new NotFoundCustomException(NotFoundCustomExceptionType.COMPANY)
+      if (!companyExists) {
+        throw new NotFoundCustomException(NotFoundCustomExceptionType.COMPANY);
       }
-      
-      return await this.companyRepository.findBy({id: id})
 
-    }catch(exception){
-      HandleException.exception(exception)
+      return await this.companyRepository.findBy({ id: id });
+    } catch (exception) {
+      HandleException.exception(exception);
     }
-  }
+  };
+
+  update = async (updateCompanyDTO: UpdateCompanyDTO) => {
+    try {
+      const company = await this.companyRepository.findOne({
+        where: { id: updateCompanyDTO.id },
+      });
+
+      if (!company) {
+        throw new NotFoundCustomException(NotFoundCustomExceptionType.COMPANY);
+      }
+
+      company.name = updateCompanyDTO.name;
+      company.address = updateCompanyDTO.address;
+      company.cellular = updateCompanyDTO.cellular;
+      company.contact = updateCompanyDTO.contact;
+      company.email = updateCompanyDTO.email;
+      company.extension = updateCompanyDTO.extension;
+      company.logo = updateCompanyDTO.logo;
+      company.phone = updateCompanyDTO.phone;
+      company.position = updateCompanyDTO.position;
+      company.rfc = updateCompanyDTO.rfc;
+      company.status = updateCompanyDTO.status;
+      company.updatedAt = new Date();
+
+      const emailIsNotUnique = await this.companyRepository.findOne({
+        where: { id: Not(company.id), email: company.email },
+      });
+      const rfcIsNotUnique = await this.companyRepository.findOne({
+        where: { id: Not(company.id), rfc: company.rfc },
+      });
+
+      if (emailIsNotUnique || rfcIsNotUnique) {
+        throw new ValidationException(ValidationExceptionType.DUPLICATE_RECORD);
+      }
+
+      return await this.companyRepository.save(company);
+    } catch (exception) {
+      console.log(exception);
+      HandleException.exception(exception);
+    }
+  };
 }
