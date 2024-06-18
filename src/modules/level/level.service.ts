1bd53c70d22b9fb1a0787ff8b293ea099e6cc7e2
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-06-17 19:34:45 -0600

    Create level service (#45)

--- src/modules/level/level.service.ts
+++ src/modules/level/level.service.ts
@@ -5,15 +5,64 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { LevelEntity } from './entities/level.entity';
 import { Repository } from 'typeorm';
 import { HandleException } from 'src/common/exceptions/handler/handle.exception';
-import { NotFoundCustomException, NotFoundCustomExceptionType } from 'src/common/exceptions/types/notFound.exception';
+import {
+  NotFoundCustomException,
+  NotFoundCustomExceptionType,
+} from 'src/common/exceptions/types/notFound.exception';
+import { UsersService } from '../users/users.service';
+import { SiteService } from '../site/site.service';
 
 @Injectable()
 export class LevelService {
-  constructor(@InjectRepository(LevelEntity) private readonly levelRepository: Repository<LevelEntity>){}
+  constructor(
+    @InjectRepository(LevelEntity)
+    private readonly levelRepository: Repository<LevelEntity>,
+    private readonly usersService: UsersService,
+    private readonly siteService: SiteService,
+  ) {}
 
   findSiteLevels = async (siteId: number) => {
+    try {
+      return await this.levelRepository.findBy({ siteId: siteId });
+    } catch (exception) {
+      HandleException.exception(exception);
+    }
+  };
+  create = async (createLevelDTO: CreateLevelDto) => {
+    try {
+      const responsible = await this.usersService.findById(createLevelDTO.responsibleId);
+      const site = await this.siteService.findById(createLevelDTO.siteId);
+      if (!responsible) {
+        throw new NotFoundCustomException(NotFoundCustomExceptionType.USER);
+      } else if (!site) {
+        throw new NotFoundCustomException(NotFoundCustomExceptionType.SITE);
+      }
+
+      createLevelDTO.companyId = site.companyId
+      createLevelDTO.responsibleName = responsible.name
+      createLevelDTO.createdAt = new Date()
+
+      if(createLevelDTO.superiorId){
+        createLevelDTO.level = await this.getActualLevelBySuperiorId(createLevelDTO.superiorId)
+      }
+
+      return await this.levelRepository.save(createLevelDTO)
+    } catch (exception) {
+      console.log(exception)
+      HandleException.exception(exception);
+    }
+  };
+
+  getActualLevelBySuperiorId = async (superiorId: number) => {
     try{
-      return await this.levelRepository.findBy({siteId: siteId})
+      const superiorLevel = await this.levelRepository.findOne({where: {id: superiorId}, select: ['level']})
+      if(!superiorLevel){
+        throw new NotFoundCustomException(NotFoundCustomExceptionType.LEVELS)
+      }
+      
+      const actualLevel = Number(superiorLevel.level)+1
+
+      return actualLevel
     }catch(exception){
       HandleException.exception(exception)
     }
