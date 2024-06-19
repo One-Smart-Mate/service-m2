24042db242fb64d110aa8c165c550b3a468f152a
isai3 <20223tn137@utez.edu.mx>
2024-06-19 11:16:44 -0600

    get card by id and its evidences

--- src/modules/card/card.module.ts
+++ src/modules/card/card.module.ts
@@ -3,9 +3,10 @@ import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CardEntity } from './entities/card.entity';
+import { EvidenceEntity } from '../evidence/entities/evidence.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CardEntity])],
+  imports: [TypeOrmModule.forFeature([CardEntity, EvidenceEntity])],
   controllers: [CardController],
   providers: [CardService],
 })
