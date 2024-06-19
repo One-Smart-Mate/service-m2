eca958d850ca1a805076f904b585561122f24668
isai3 <20223tn137@utez.edu.mx>
2024-06-19 11:46:32 -0600

    adds levelName in response

--- src/modules/card/card.service.ts
+++ src/modules/card/card.service.ts
@@ -7,36 +7,56 @@ import { EvidenceEntity } from '../evidence/entities/evidence.entity';
 
 @Injectable()
 export class CardService {
-  constructor(@InjectRepository(CardEntity) private readonly cardRepository: Repository<CardEntity>,
-  @InjectRepository(EvidenceEntity) private readonly evidenceRepository: Repository<EvidenceEntity>){}
+  constructor(
+    @InjectRepository(CardEntity)
+    private readonly cardRepository: Repository<CardEntity>,
+    @InjectRepository(EvidenceEntity)
+    private readonly evidenceRepository: Repository<EvidenceEntity>,
+  ) {}
 
   findSiteCards = async (siteId: number) => {
-    try{
-      return await this.cardRepository.findBy({siteId: siteId})
-    }catch(exception){
-      HandleException.exception(exception)
-    }
-  }
-  findResponsibleCards = async (responsibleId: number) => {
-    try{
-      return await this.cardRepository.findBy({responsableId: responsibleId})
-    }catch(exception){
-      HandleException.exception(exception)
-    }
-  }
-
-  findCardByIDAndGetEvidences = async (cardId: number) =>{
-    try{
-      const card = await this.cardRepository.findOneBy({id: cardId})
-      if(card) card['levelName'] = card.areaName
-      const evidences = await this.evidenceRepository.findBy({cardId: cardId})
-
-      return{
-        card,
-        evidences
+    try {
+      const cards = await this.cardRepository.findBy({ siteId: siteId });
+      if (cards) {
+        cards.forEach((card) => {
+          card['levelName'] = card.areaName;
+        });
       }
-    }catch(exception){
-      HandleException.exception(exception)
+      return cards;
+    } catch (exception) {
+      HandleException.exception(exception);
     }
-  } 
+  };
+  findResponsibleCards = async (responsibleId: number) => {
+    try {
+      const cards = await this.cardRepository.findBy({
+        responsableId: responsibleId,
+      });
+      if (cards) {
+        cards.forEach((card) => {
+          card['levelName'] = card.areaName;
+        });
+      }
+      return cards;
+    } catch (exception) {
+      HandleException.exception(exception);
+    }
+  };
+
+  findCardByIDAndGetEvidences = async (cardId: number) => {
+    try {
+      const card = await this.cardRepository.findOneBy({ id: cardId });
+      if (card) card['levelName'] = card.areaName;
+      const evidences = await this.evidenceRepository.findBy({
+        cardId: cardId,
+      });
+
+      return {
+        card,
+        evidences,
+      };
+    } catch (exception) {
+      HandleException.exception(exception);
+    }
+  };
 }
