f1bcbd2b0a50dc7006237a56682f1701c3d7432a
isai3 <20223tn137@utez.edu.mx>
2024-07-08 15:33:15 -0600

    get card by superior Id and site Id

--- src/modules/card/card.controller.ts
+++ src/modules/card/card.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { CardService } from './card.service';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateCardDTO } from './models/dto/create-card.dto';
@@ -11,36 +20,48 @@ export class CardController {
   constructor(private readonly cardService: CardService) {}
 
   @Get('/all/:siteId')
-  @ApiParam({name: 'siteId'})
-  findBySiteId(@Param('siteId') siteId: number){
-    return this.cardService.findSiteCards(siteId)
+  @ApiParam({ name: 'siteId' })
+  findBySiteId(@Param('siteId') siteId: number) {
+    return this.cardService.findSiteCards(siteId);
   }
   @Get('/responsible/:responsibleId')
-  @ApiParam({name: 'responsibleId'})
-  findByResponsibleId(@Param('responsibleId') responsibleId: number){
-    return this.cardService.findSiteCards(responsibleId)
+  @ApiParam({ name: 'responsibleId' })
+  findByResponsibleId(@Param('responsibleId') responsibleId: number) {
+    return this.cardService.findSiteCards(responsibleId);
   }
 
   @Get('/:cardId')
-  findByIDAndGetEvidences(@Param('cardId') cardId: number){
-    return this.cardService.findCardByIDAndGetEvidences(cardId)
+  findByIDAndGetEvidences(@Param('cardId') cardId: number) {
+    return this.cardService.findCardByIDAndGetEvidences(cardId);
   }
 
   @Post('/create')
-  create(@Body() createCardDTO: CreateCardDTO){
-    return this.cardService.create(createCardDTO)
+  create(@Body() createCardDTO: CreateCardDTO) {
+    return this.cardService.create(createCardDTO);
   }
   @Put('/update/definitive-solution')
-  updateDefinitiveSolution(@Body() updateDefinitiveSolutionDTO: UpdateDefinitiveSolutionDTO){
-    return this.cardService.updateDefinitivesolution(updateDefinitiveSolutionDTO)
+  updateDefinitiveSolution(
+    @Body() updateDefinitiveSolutionDTO: UpdateDefinitiveSolutionDTO,
+  ) {
+    return this.cardService.updateDefinitivesolution(
+      updateDefinitiveSolutionDTO,
+    );
   }
   @Put('/update/provisional-solution')
-  updateProvisionalSolution(@Body() updateProvisionalSolutionDTO: UpdateProvisionalSolutionDTO){
-    return this.cardService.updateProvisionalSolution(updateProvisionalSolutionDTO)
+  updateProvisionalSolution(
+    @Body() updateProvisionalSolutionDTO: UpdateProvisionalSolutionDTO,
+  ) {
+    return this.cardService.updateProvisionalSolution(
+      updateProvisionalSolutionDTO,
+    );
   }
-  @Get('/all/zone/:superiorId')
-  @ApiParam({name: 'superiorId'})
-  getCardsZone(@Param('superiorId') superiorId: number){
-    return this.cardService.getCardBySuperiorId(superiorId)
+  @Get('/all/zone/:superiorId/:siteId')
+  @ApiParam({ name: 'superiorId' })
+  @ApiParam({ name: 'siteId' })
+  getCardsZone(
+    @Param('superiorId') superiorId: number,
+    @Param('siteId') siteId: number,
+  ) {
+    return this.cardService.getCardBySuperiorId(superiorId, siteId);
   }
 }
