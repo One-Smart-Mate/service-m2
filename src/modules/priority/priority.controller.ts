7fdc9cdc9bb7ed12d346eac7df1e8c28a63bc796
isai3 <20223tn137@utez.edu.mx>
2024-07-25 15:26:29 -0600

    Get only the active priorities and fill the deletedAt when one is deleted

--- src/modules/priority/priority.controller.ts
+++ src/modules/priority/priority.controller.ts
@@ -20,6 +20,12 @@ export class PriorityController {
 
   @Get('/all/:siteId')
   @ApiParam({ name: 'siteId', required: true, example: 1 })
+  findActivePrioritiesByCompanyId(@Param('siteId') siteId: number) {
+    return this.priorityService.findSiteActivePriorities(+siteId);
+  }
+
+  @Get('/site/:siteId')
+  @ApiParam({ name: 'siteId', required: true, example: 1 })
   findPrioritiesByCompanyId(@Param('siteId') siteId: number) {
     return this.priorityService.findSitePriorities(+siteId);
   }
@@ -31,13 +37,13 @@ export class PriorityController {
   }
 
   @Put('/update')
-  @ApiBody({ type: UpdatePriorityDTO})
-  update (@Body() updatePriorityDTO: UpdatePriorityDTO){
-    return this.priorityService.update(updatePriorityDTO)
+  @ApiBody({ type: UpdatePriorityDTO })
+  update(@Body() updatePriorityDTO: UpdatePriorityDTO) {
+    return this.priorityService.update(updatePriorityDTO);
   }
   @Get('/one/:id')
-  @ApiParam({name: 'id', example: 1})
-  findOneById(@Param('id') id: number){
-    return this.priorityService.findById(id)
+  @ApiParam({ name: 'id', example: 1 })
+  findOneById(@Param('id') id: number) {
+    return this.priorityService.findById(id);
   }
 }
