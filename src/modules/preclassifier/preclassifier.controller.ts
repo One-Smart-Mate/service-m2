8fd70bb900ce4ba4bd6e0f9345707ef429413672
isai3 <20223tn137@utez.edu.mx>
2024-07-25 15:13:51 -0600

    Get only active preclassifiers method added and deletedat

--- src/modules/preclassifier/preclassifier.controller.ts
+++ src/modules/preclassifier/preclassifier.controller.ts
@@ -10,32 +10,40 @@ export class PreclassifierController {
   constructor(private readonly preclassifierService: PreclassifierService) {}
 
   @Get('/all/:cardTypesId')
-  @ApiParam({name:'cardTypesId', required: true, example: 1})
-  findAllByCardTypes(@Param('cardTypesId') cardTypeId: number){
-    return this.preclassifierService.findCardTypesPreclassifiers(cardTypeId)
+  @ApiParam({ name: 'cardTypesId', required: true, example: 1 })
+  findAllAcviteByCardTypes(@Param('cardTypesId') cardTypeId: number) {
+    return this.preclassifierService.findCardTypesActivePreclassifiers(
+      cardTypeId,
+    );
+  }
+
+  @Get('/card-type/:cardTypesId')
+  @ApiParam({ name: 'cardTypesId', required: true, example: 1 })
+  findAllByCardTypes(@Param('cardTypesId') cardTypeId: number) {
+    return this.preclassifierService.findCardTypesPreclassifiers(cardTypeId);
   }
 
   @Get('/site/:siteId')
-  @ApiParam({name:'siteId', required: true, example: 1})
-  findAllBySite(@Param('siteId') siteId: number){
-    return this.preclassifierService.findSitePreclassifiers(siteId)
+  @ApiParam({ name: 'siteId', required: true, example: 1 })
+  findAllActiveBySite(@Param('siteId') siteId: number) {
+    return this.preclassifierService.findSiteActivePreclassifiers(siteId);
   }
 
   @Get('/:id')
-  @ApiParam({name:'id', required: true, example: 1})
-  findOneById(@Param('id') siteId: number){
-    return this.preclassifierService.findById(siteId)
+  @ApiParam({ name: 'id', required: true, example: 1 })
+  findOneById(@Param('id') siteId: number) {
+    return this.preclassifierService.findById(siteId);
   }
 
   @Post('/create')
-  @ApiBody({type: CreatePreclassifierDTO})
-  create(@Body() createPreclassifierDTO: CreatePreclassifierDTO){
-    return this.preclassifierService.create(createPreclassifierDTO)
+  @ApiBody({ type: CreatePreclassifierDTO })
+  create(@Body() createPreclassifierDTO: CreatePreclassifierDTO) {
+    return this.preclassifierService.create(createPreclassifierDTO);
   }
 
   @Put('/update')
-  @ApiBody({type: UpdatePreclassifierDTO})
-  update(@Body() updatePreclassifierDTO:UpdatePreclassifierDTO){
-    return this.preclassifierService.update(updatePreclassifierDTO)
+  @ApiBody({ type: UpdatePreclassifierDTO })
+  update(@Body() updatePreclassifierDTO: UpdatePreclassifierDTO) {
+    return this.preclassifierService.update(updatePreclassifierDTO);
   }
 }
