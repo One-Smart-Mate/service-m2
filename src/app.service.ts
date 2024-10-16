cd29728bbf3179c9f2b7e27407975b08baad40f4
Immanuel Diaz <immanuel.diaz@includedhealth.com>
2024-10-16 13:53:14 -0600

    refactor vars

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,8 @@ import { stringConstants } from './utils/string.constant';
 @Injectable()
 export class AppService {
   getAppInfo(): { app_version: string } {
+    console.log("ENV")
+    console.log(process.env)
     return {
       app_version: process.env.APP_ENV,
     };
