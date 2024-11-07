b4ec56e3316d837f27b8ed2bfc06c9bde4ca1be2
isai3 <20223tn137@utez.edu.mx>
2024-11-07 13:01:50 -0600

    Revert "libraries updated"
    This reverts commit fdfff232d7f471b4bfc4bef258fc7bc0d4210142.

--- src/config/type.orm.config.ts
+++ src/config/type.orm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 const typeOrmConfig = TypeOrmModule.forRoot({
   type: 'mysql',
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'nJpPjMXrgTVYAfgYmwoiusECdFSPSCEm',
-  port: parseInt(process.env.DB_PORT) || 25045,
-  database: process.env.DB_NAME || 'railway',
-  host: process.env.DB_HOST || 'autorack.proxy.rlwy.net',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   autoLoadEntities: true,
