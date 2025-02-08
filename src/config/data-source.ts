034814923581ef60c82a34f34a55efee29959f59
Isaacjbar <isaac777jb@gmail.com>
2025-02-07 18:39:01 -0600

    feat: Change credentials

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,21 +1,21 @@
 import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
-  type: 'mysql', // O el tipo de base de datos que estés usando
-  host: 'autorack.proxy.rlwy.net',
-  port: parseInt('25045'),
-  username: 'root',
-  password: 'nJpPjMXrgTVYAfgYmwoiusECdFSPSCEm',
-  database: 'railway',
-  synchronize: false, // Evita sincronización automática
-  dropSchema: false, // Asegura que no elimine el esquema
-  logging: false, // O 'all' si quieres ver los logs de las operaciones
-  logger: 'file', // Guarda logs en archivo
+  type: 'mysql', 
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false, 
+  dropSchema: false, 
+  logging: false, 
+  logger: 'file', 
   entities: ['./dist' + '/**/*.entity{.ts,.js}'],
   migrations: ['./dist'+ '/migrations/*{.ts,.js}'],
-  migrationsTableName: 'migration_table', // Nombre de la tabla de control de migraciones
-  migrationsRun: false, // Evita que corra automáticamente las migraciones
-  charset: 'utf8mb4_general_ci', // Configura el charset si usas MySQL
+  migrationsTableName: 'migration_table', 
+  migrationsRun: false, 
+  charset: 'utf8mb4_general_ci', 
 });
 
 export default AppDataSource;
