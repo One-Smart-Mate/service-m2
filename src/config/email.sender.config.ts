7e09ba4aa84ae4447b1fb44d644a249052617f82
Isaac Jimenez <120062171+Isaacjbar@users.noreply.github.com>
2025-05-10 15:13:05 -0600

    feat: Corrections

--- src/config/email.sender.config.ts
+++ src/config/email.sender.config.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 const mailConfig = MailerModule.forRoot({
   transport: {
     host: process.env.MAIL_HOST,
-    port: parseInt(process.env.MAIL_PORT, 10),
+    port: parseInt(process.env.MAIL_PORT || '456', 10),
     secure: true,
     auth: {
       user: process.env.MAIL_USER,
@@ -13,7 +13,7 @@ const mailConfig = MailerModule.forRoot({
     },
   },
   defaults: {
-    from: `"OSM" <${process.env.MAIL_FROM}>`,
+    from: process.env.MAIL_FROM,
   },
   template: {
     dir: join(__dirname, 'templates'),
