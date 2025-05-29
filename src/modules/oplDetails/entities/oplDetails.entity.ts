e0b5e914386f63c6bde8569c7760053137341b27
Isaac Jimenez <120062171+Isaacjbar@users.noreply.github.com>
2025-05-29 16:17:18 -0600

    feat: CILT 3.3

--- src/modules/oplDetails/entities/oplDetails.entity.ts
+++ src/modules/oplDetails/entities/oplDetails.entity.ts
@@ -1,12 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column,
+  ManyToOne,
+  JoinColumn
+} from 'typeorm';
+import { OplMstr } from '../../oplMstr/entities/oplMstr.entity';
+import { SiteEntity } from '../../site/entities/site.entity';
 
 @Entity("opl_details")
 export class OplDetailsEntity {
-  @PrimaryGeneratedColumn({ type: "int", name: "id" })
+  @PrimaryGeneratedColumn({ type: "int", name: "id", unsigned: true })
   id: number;
 
-  @Column("int", { name: "opl_id" })
+  @Column("int", { name: "site_id", nullable: true, unsigned: true })
+  siteId: number | null;
+
+  @Column("int", { name: "opl_id", unsigned: true })
   oplId: number;
 
   @Column("tinyint", { name: "order" })
@@ -21,10 +31,29 @@ export class OplDetailsEntity {
   @Column("varchar", { name: "media_url", nullable: true, length: 500 })
   mediaUrl: string | null;
 
-  @Column("datetime", {
+  @Column("timestamp", {
+    name: "created_at",
+    nullable: true,
+    default: () => "CURRENT_TIMESTAMP"
+  })
+  createdAt: Date | null;
+
+  @Column("timestamp", {
     name: "updated_at",
     nullable: true,
     default: () => "CURRENT_TIMESTAMP",
+    onUpdate: "CURRENT_TIMESTAMP"
   })
   updatedAt: Date | null;
+
+  @Column("timestamp", { name: "deleted_at", nullable: true })
+  deletedAt: Date | null;
+
+  @ManyToOne(() => OplMstr)
+  @JoinColumn({ name: 'opl_id' })
+  opl: OplMstr;
+
+  @ManyToOne(() => SiteEntity)
+  @JoinColumn({ name: 'site_id' })
+  site: SiteEntity;
 }
--- src/modules/oplLevels/models/create-opl-levels.dto.ts
+++ src/modules/oplLevels/models/create-opl-levels.dto.ts
@@ -1,7 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, IsOptional, IsISO8601 } from 'class-validator';
 
 export class CreateOplLevelsDTO {
+  @ApiProperty({ description: 'Site ID', example: 1, required: false })
+  @IsOptional()
+  @IsNumber()
+  siteId?: number;
+
   @ApiProperty({ description: 'Opl ID', example: 1 })
   @IsNotEmpty()
   @IsNumber()
@@ -11,4 +16,8 @@ export class CreateOplLevelsDTO {
   @IsNotEmpty()
   @IsNumber()
   levelId: number;
+
+  @ApiProperty({ description: 'Creation date in ISO format (YYYY-MM-DDTHH:mm:ss.sssZ)', default: '2023-06-20T00:00:00.000Z' })
+  @IsISO8601()
+  createdAt: string;
 } 
