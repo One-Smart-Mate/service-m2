8f26a4c1ce91b59de73cee1e2ee440a9fc2035b2
Isaac Jimenez <120062171+Isaacjbar@users.noreply.github.com>
2025-07-07 21:09:05 -0600

    feat: Type of evidence

--- src/modules/CiltSequencesExecutions/models/dto/create.evidence.dto.ts
+++ src/modules/CiltSequencesExecutions/models/dto/create.evidence.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString, IsDateString } from 'class-validator';
+import { IsNumber, IsString, IsDateString, IsOptional, IsEnum } from 'class-validator';
+import { CiltSequencesExecutionsEvidencesType } from 'src/modules/CiltSequencesExecutionsEvidences/models/dtos/createCiltSequencesEvidence.dto';
 
 export class CreateEvidenceDTO {
   @ApiProperty({ description: 'Execution ID', example: 1 })
@@ -9,6 +10,15 @@ export class CreateEvidenceDTO {
   @ApiProperty({ description: 'Evidence URL', example: 'https://example.com/evidence/image.jpg' })
   @IsString()
   evidenceUrl: string;
+
+  @ApiProperty({ 
+    description: 'Type of evidence', 
+    required: false,
+    example: 'INITIAL'
+  })
+  @IsOptional()
+  @IsEnum(CiltSequencesExecutionsEvidencesType)
+  type?: CiltSequencesExecutionsEvidencesType;
 
   @ApiProperty({ description: 'Created date', example: '2023-06-20T00:00:00.000Z' })
   @IsDateString()
