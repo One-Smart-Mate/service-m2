356bd39506fef49f12abf3ad9681973dc5590e94
Immanuel Diaz <immanuel.diaz@includedhealth.com>
2025-09-12 11:14:50 -0600

    fix(api) update service and deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,11 @@ import { stringConstants } from './utils/string.constant';
 
 @Injectable()
 export class AppService {
-  getAppInfo(): { app_version: string } {
-    console.log("ENV")
-    console.log(process.env)
+  getAppInfo() {
     return {
-      app_version: process.env.APP_ENV,
+      date: new Date().toISOString(),
+      app_version: process.env.APP_VERSION || "env not found",
+      deploy_env: process.env.DEPLOY_ENV || "deploy env not found",
     };
   }
 }
